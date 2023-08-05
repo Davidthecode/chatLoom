22e9b2b2d17c2870819447b77e6105bfec6753b4
David Ajibola <ajiboladavid0963@gmail.com>
2023-08-05 14:20:59 +0100

    feat:UI fix

---
+++ src/app/chatloom/page.tsx
@@ -0,0 +1,31 @@
+export default function SignIn() {
+  return (
+    <div>
+      <aside>
+        
+        <section>
+          <h2>UserName</h2>
+        </section>
+
+        <section>
+            <h2>Create a group</h2>
+        </section>
+
+        <section>
+          <div>
+            <input type="text"  className="border-2"/>
+          </div>
+          <div>
+            <h2>users</h2>
+          </div>
+        </section>
+
+        <section>
+          <h3>settings</h3>
+        </section>
+        
+      </aside>
+
+    </div>
+  )
+}
