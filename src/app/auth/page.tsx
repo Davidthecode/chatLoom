ded138d90bbbda28966e162882a755e079b6990e
David Ajibola <ajiboladavid0963@gmail.com>
2023-08-05 14:20:29 +0100

    feat:UI fix

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import {auth, provider} from '../../../firebase'
+import {signInWithPopup} from 'firebase/auth'
+import {setCookie} from 'cookies-next'
+
+export default function Auth ({setIsAuth}: any) {
+
+  const handleSignIn = async()=> {
+    try {
+      const result = await signInWithPopup(auth, provider)
+      setCookie('auth-token', result.user.refreshToken )
+      setIsAuth(true)
+      
+    } catch (error) {
+      console.log(error);  
+    }
+  }
+
+    return(
+      <div>
+        <h1>SignIn with Google</h1>
+        <button className='border' onClick={handleSignIn}>sign in</button>
+      </div>
+    )
+  }
