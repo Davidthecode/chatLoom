b275435560d489f2dbd002a692af8f2f79088ab4
David Ajibola <ajiboladavid0963@gmail.com>
2023-08-05 14:22:21 +0100

    feat:UI fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import Auth from "./auth/page"
+import Chatloom from "./chatloom/page"
+import { useState } from "react"
+
 export default function Home () {
+  const [isAuth, setIsAuth] = useState(false)
+  
+  if(!isAuth){
+    return(
+      <Auth
+      setIsAuth={setIsAuth}
+      />
+    )
+  }
+
   return(
     <div>
-      <h1>ChatLoom</h1>
+      <Chatloom />
     </div>
   )
 }
